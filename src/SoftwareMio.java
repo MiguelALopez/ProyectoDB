048e0efc6257ea5a97aced6766c3c6a27ed21397
miguelALopez <miguel123dr@gmail.com>
2015-10-03 21:02:06 -0500

    Creado el Main del programa

---
+++ src/SoftwareMio.java
@@ -0,0 +1,17 @@
+/**
+ * ********************************************
+ * Autor: Camilo Ruiz Casanova - 1324486
+ * Autor: Miguel Angel Lopez - 1326691
+ * Autor: Andres Felipe Polanco - 1324539
+ * Fecha: 03-oct-2015
+ * Nombre del Archivo: SoftwareMio.java
+ * Plan: Ingeniería de Sistemas - 3743
+ * Institución Educativa: Universidad del Valle (Cali - Colombia)
+ * *********************************************
+ */
+
+public class SoftwareMio {
+    public static void main(String[] args) {
+
+    }
+}
